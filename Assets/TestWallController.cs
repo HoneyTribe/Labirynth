918390588a4b7e72158e5fe355cce53cc9ca6b36
endrju3 <endrju3@o2.pl>
2014-04-03 23:23:38 +0100

    Added testWalls

---
+++ Assets/TestWallController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestWallController : MonoBehaviour {
+
+	public Texture2D texture;
+	private Vector3 localScale = Vector3.zero;
+
+	void Update ()
+	{
+		if (!localScale.Equals(gameObject.transform.localScale))
+		{
+			float ratio = gameObject.transform.localScale.x /  gameObject.transform.localScale.y;
+			
+			gameObject.renderer.material.mainTexture = texture;
+			gameObject.renderer.material.mainTextureScale = new Vector2 (ratio, 1);
+
+			localScale = gameObject.transform.localScale;
+		}
+
+	}
+}
