2a07e63f3e7374f8abca0784f46993a6dd884b31
endrju3 <endrju3@o2.pl>
2014-04-07 23:57:43 +0100

    Added bloody earthquake

---
+++ Assets/CameraEarthquakeController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraEarthquakeController : MonoBehaviour {
+
+	public float startingShakeDistance = 0.8f;
+	public float decreasePercentage = 0.5f;
+	public float shakeSpeed = 50f;
+	public int numberOfShakes = 10;
+		
+	private Vector3 originalPosition;
+	private int shake;
+	private float shakeDistance;
+	private float hitTime;
+
+	void Start()
+	{
+		originalPosition = camera.transform.position;
+		StartCoroutine(StartEarthquakeTimer());
+	}
+
+	IEnumerator StartEarthquakeTimer() 
+	{
+		while (true)
+		{
+			int delay = Random.Range (30, 40);
+			yield return new WaitForSeconds (delay);
+			shake = numberOfShakes;
+			shakeDistance = startingShakeDistance;
+			hitTime = Time.time;
+		}
+	}
+
+	void Update()
+	{
+		if (shake > 0)
+		{
+			float timer = (Time.time - hitTime) * shakeSpeed;
+			camera.transform.position = new Vector3(originalPosition.x + Mathf.Sin(timer) * shakeDistance,
+			                                        originalPosition.y,
+			                                        originalPosition.z);
+
+			if (timer > Mathf.PI * 2)
+			{
+				hitTime = Time.time;
+				shakeDistance *= decreasePercentage;
+				shake--;
+			}
+		}
+
+		if (shake == 0)
+		{
+			camera.transform.position = originalPosition;
+			shake--;
+		}
+	}
+}
