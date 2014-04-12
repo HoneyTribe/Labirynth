cafd8a296bb505fa72b0770e9b8a69adea455215
HoneyTribe <shaz@honeytribestudios.com>
2014-04-12 15:46:31 +0100

    Max angle for light rotation increased.

--- Assets/LightController.cs
+++ Assets/LightController.cs
@@ -15,7 +15,7 @@ public class LightController : MonoBehaviour {
 
 	void MoveLeft()
 	{
-		if (transform.rotation.y > -0.40) 
+		if (transform.rotation.y > -0.45) 
 		{
 			transform.Rotate (0, -rotationSpeed * Time.deltaTime, 0);
 		}
@@ -23,7 +23,7 @@ public class LightController : MonoBehaviour {
 
 	void MoveRight()
 	{
-		if (transform.rotation.y < 0.40) 
+		if (transform.rotation.y < 0.45) 
 		{
 			transform.Rotate (0, rotationSpeed * Time.deltaTime, 0);
 		}
