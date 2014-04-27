1897f1db84a6027800f84a457ed20975db7ebdcc
HoneyTribe <shaz@honeytribestudios.com>
2014-04-28 00:11:56 +0100

    Levels added

--- Assets/GameControl/LevelDefinition.cs
+++ Assets/GameControl/LevelDefinition.cs
@@ -25,7 +25,46 @@ namespace AssemblyCSharp
 					levels1.Add(new Level(1,new List<MonsterTemplate>{},15,15,6,6));
 
 		 	    // 2 player levels
-					levels2.Add(new Level(2,new List<MonsterTemplate>{},15,15,6,6));	
+					levels3.Add(new Level(1,new List<MonsterTemplate>{},15,15,6,6));
+					levels3.Add(new Level(1,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 2.4f)
+					},5,15,7,7));
+					levels3.Add(new Level(2,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 2.6f),
+						new MonsterTemplate("Standard", 2.6f)
+					},5,6,8,8));
+					levels3.Add(new Level(2,new List<MonsterTemplate>{},10,15,6,6));
+					levels3.Add(new Level(2,new List<MonsterTemplate>{
+						new MonsterTemplate("Flying", 2.0f)
+					},5,6,7,7));
+					levels3.Add(new Level(3,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 3.0f),
+						new MonsterTemplate("Flying", 2.4f)
+					},5,6,8,8));
+					levels3.Add(new Level(4,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 3.4f),
+						new MonsterTemplate("Standard", 3.4f),
+						new MonsterTemplate("Flying", 2.0f)
+					},5,6,9,9));
+					levels3.Add(new Level(4,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 3.6f),
+						new MonsterTemplate("Standard", 3.6f),
+						new MonsterTemplate("Standard", 3.6f),
+						new MonsterTemplate("Standard", 3.6f)
+					},5,6,9,9));
+					levels3.Add(new Level(5,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 3.8f),
+						new MonsterTemplate("Flying", 2.2f),
+						new MonsterTemplate("Standard", 3.8f),
+						new MonsterTemplate("Flying", 3.8f)
+					},5,10,10,10));
+					levels3.Add(new Level(5,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 4.0f),
+						new MonsterTemplate("Standard", 4.0f),
+						new MonsterTemplate("Flying", 2.4f),
+						new MonsterTemplate("Standard", 4.0f),
+						new MonsterTemplate("Flying", 2.4f)
+					},5,10,10,10));	
 				/*
 					levels.Add(new Level(1,new List<MonsterTemplate>{},15,15,6,6));
 					levels.Add(new Level(1,new List<MonsterTemplate>{
@@ -113,7 +152,46 @@ namespace AssemblyCSharp
 						},5,10,10,10));
 
 					// 4 player levels	
-					levels4.Add(new Level(2,new List<MonsterTemplate>{},15,15,6,6));
+					levels3.Add(new Level(3,new List<MonsterTemplate>{},15,15,6,6));
+					levels3.Add(new Level(3,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 2.4f)
+					},5,15,7,7));
+					levels3.Add(new Level(4,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 2.6f),
+						new MonsterTemplate("Standard", 2.6f)
+					},5,6,8,8));
+					levels3.Add(new Level(4,new List<MonsterTemplate>{},10,15,6,6));
+					levels3.Add(new Level(4,new List<MonsterTemplate>{
+						new MonsterTemplate("Flying", 2.0f)
+					},5,6,7,7));
+					levels3.Add(new Level(5,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 3.0f),
+						new MonsterTemplate("Flying", 2.4f)
+					},5,6,8,8));
+					levels3.Add(new Level(6,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 3.4f),
+						new MonsterTemplate("Standard", 3.4f),
+						new MonsterTemplate("Flying", 2.0f)
+					},5,6,9,9));
+					levels3.Add(new Level(6,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 3.6f),
+						new MonsterTemplate("Standard", 3.6f),
+						new MonsterTemplate("Standard", 3.6f),
+						new MonsterTemplate("Standard", 3.6f)
+					},5,6,9,9));
+					levels3.Add(new Level(7,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 3.8f),
+						new MonsterTemplate("Flying", 2.2f),
+						new MonsterTemplate("Standard", 3.8f),
+						new MonsterTemplate("Flying", 3.8f)
+					},5,10,10,10));
+					levels3.Add(new Level(7,new List<MonsterTemplate>{
+						new MonsterTemplate("Standard", 4.0f),
+						new MonsterTemplate("Standard", 4.0f),
+						new MonsterTemplate("Flying", 2.4f),
+						new MonsterTemplate("Standard", 4.0f),
+						new MonsterTemplate("Flying", 2.4f)
+					},5,10,10,10));
 				}
 
 				public List<Level> getLevels(int numOfPlayers)
