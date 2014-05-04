088279227e9c0d79fe4e7f1d509b3751524b14ae
endrju3 <endrju3@o2.pl>
2014-05-04 20:41:47 +0100

    Revert "Another version of portal"
    This reverts commit 474ea7665b4f135ddea313753964e8c20f210bf0.

--- Assets/PortalGun/PortalController.cs
+++ Assets/PortalGun/PortalController.cs
@@ -1,27 +1,19 @@
 using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 
 public class PortalController : MonoBehaviour {
 
-	private static float speed = 10f;
-	private static float height = 5f;
-	private static float timeUp = height/speed;
-
 	private PortalController theOtherPortal;
 	private bool settled;
-	private int state;
-	private float time;
-
-	private List<GameObject> closeObjects = new List<GameObject> ();
 
 	public void setTheOtherPortal(PortalController theOtherPortal)
 	{
 		this.theOtherPortal = theOtherPortal;
 	}
 
-	public bool hasCloseObjects()
+	public void setSettled()
 	{
-		return closeObjects.Count != 0;
+		this.settled = true;
 	}
 
 	public bool isSettled()
@@ -29,79 +21,30 @@ public class PortalController : MonoBehaviour {
 		return this.settled;
 	}
 
-	void Update()
-	{
-		if ((state == 0) && 
-		    (theOtherPortal != null) && 
-		    (isSettled()) && (theOtherPortal.isSettled()) &&
-		    ((hasCloseObjects()) || (theOtherPortal.hasCloseObjects()))
-		   )
-		{
-			state++;
-			time = 0f;
-			foreach (GameObject obj in closeObjects)
-			{
-				obj.rigidbody.useGravity = false;
-				obj.rigidbody.velocity = new Vector3 (0, speed, 0);
-				if ((obj.tag == "Player") || (obj.tag == "Monster"))
-				{
-					obj.gameObject.SendMessage("setStopped", true);
-				}
-			}
-		}
-
-		if (state == 1)
-		{
-			time += Time.deltaTime;
-
-			if (time > timeUp)
-			{
-				foreach (GameObject obj in closeObjects)
-				{
-					if ((obj.tag == "Player") || (obj.tag == "Monster"))
-					{
-						Vector3 move = theOtherPortal.transform.position - transform.position;
-						obj.transform.position = new Vector3(obj.transform.position.x + move.x,
-						                                     height,
-						                                     obj.transform.position.z + move.z);
-						obj.rigidbody.useGravity = true;
-						obj.rigidbody.velocity = new Vector3(0, -speed, 0);
-
-						if (obj.tag == "Monster")
-						{
-							obj.gameObject.SendMessage("Recalculate");
-						}
-					}
-				}
-				Destroy (gameObject.gameObject);
-			}
-		}
-	}
-
 	void OnTriggerEnter(Collider collider)
 	{
-		if (state == 0)
+		if (collider.name == "Ground")
 		{
-			if (collider.name == "Ground")
+			settled = true;
+		}
+		else
+		{
+			if ((theOtherPortal != null) && (isSettled()) && (theOtherPortal.isSettled()))
 			{
-				settled = true;
-			}
-
-			if ((collider.tag == "Monster") || (collider.tag == "Item") || (collider.tag == "Player"))
-			{
-				if (!closeObjects.Contains(collider.gameObject))
+				if ((collider.tag == "Player") || (collider.tag == "Monster"))
 				{
-					closeObjects.Add (collider.gameObject);
+					Vector3 centralPosition = AssemblyCSharp.Instantiation.instance.getCentralPosition(theOtherPortal.transform.position);
+					Vector3 pointBetween = Vector3.Lerp(centralPosition, theOtherPortal.transform.position, 0.5f);
+					collider.transform.position = new Vector3(pointBetween.x, collider.transform.position.y, pointBetween.z);
+					Destroy (theOtherPortal.gameObject);
+					Destroy (gameObject.gameObject);
+
+					if (collider.tag == "Monster")
+					{
+						collider.gameObject.SendMessage("Recalculate");
+					}
 				}
 			}
-		}
-	}
-
-	void OnTriggerExit(Collider collider)
-	{
-		if (state == 0)
-		{
-			closeObjects.Remove (collider.gameObject);
 		}
 	}
 }
