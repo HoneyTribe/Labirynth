6ac6427b4b16c8bb81cc3f73a4ddd9b51f71cd2a
HoneyTribe <shaz@honeytribestudios.com>
2014-06-02 15:37:59 +0100

    Various adjustments
    TopLight: Culling mask and angle. Now easier to see edges of map.
    Light: Adjusted position and size of trigger rectangle.
    LightForKey: Culling Mask. Added monsters.
    Level 2: adjusted intro and levelDefinition (2 player).

--- Assets/GameControl/LevelDefinition.cs
+++ Assets/GameControl/LevelDefinition.cs
@@ -36,10 +36,11 @@ namespace AssemblyCSharp
 					//1
 					levels2.Add(new Level(1,new List<MonsterTemplate>{},15,15,6,6, null, null));
 					//2
-					levels2.Add(new Level(15,new List<MonsterTemplate>{},0,0,9,9, null, "First"));
+					levels2.Add(new Level(5,new List<MonsterTemplate>{},0,0,8,8, null, "First"));
 					//levels2.Add(new Level(1,new List<MonsterTemplate>{
 					//	new MonsterTemplate("Standard", 2.4f)
 					//},5,15,7,7, null, null));
+
 					//3
 					levels2.Add(new Level(2,new List<MonsterTemplate>{
 						new MonsterTemplate("Standard", 2.4f)
--- Assets/LevelIntroduction/Level2.cs
+++ Assets/LevelIntroduction/Level2.cs
@@ -7,14 +7,15 @@ public class Level2 : LevelSetup {
 	{
 		List<Action> actions = new List<Action>();
 		
-		actions.Add(new CreateMonsterAction("Standard"));
-		actions.Add(new MoveCameraAction("Monster"));
-		actions.Add(new TextAction(4, "Watch out for monsters!"));
+		//actions.Add(new CreateMonsterAction("Standard"));
+		//actions.Add(new MoveCameraAction("Monster"));
 		actions.Add(new MoveCameraAction("Player2"));
+		actions.Add(new TextAction(4, "Watch out for monsters!"));
+		actions.Add(new MoveCameraAction("Device"));
 		actions.Add(new TextAction(4, "When in the maze: Tap your trigger button to drop the decoy."));
-		actions.Add(new MoveCameraAction("Player1"));
-		actions.Add(new TextAction(4, "When in the light-machine: Aim at monsters and tap your trigger button. Zap to distract!"));
 		actions.Add(new MoveCameraAction("Lighthouse"));
+		actions.Add(new TextAction(4, "When in the light-machine: Aim at monsters and tap your trigger button. Zap to distract!"));
+		actions.Add(new MoveCameraAction("Player1"));
 		actions.Add(new TextAction(4, "Each time you zap monsters the power is depleted."));
 
 		return actions;
