8617bfa814ee338fd0c8439b2297483c331d43be
endrju3 <endrju3@o2.pl>
2014-06-15 13:39:29 +0100

    First attempt to billboarding

---
+++ Assets/Fusion/PuzzlePieceController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuzzlePieceController : MonoBehaviour {
+
+	private GameObject myCamera;
+
+	void Start()
+	{
+		myCamera = GameObject.Find ("MainCamera_Front");
+	}
+
+	void Update()
+	{
+	
+	}
+
+}
