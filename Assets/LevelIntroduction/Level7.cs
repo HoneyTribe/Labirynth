b32a0e18766c23837b1ff9ba1b54b144728f782b
HoneyTribe <shaz@honeytribestudios.com>
2014-06-17 21:03:39 +0100

    level intro

---
+++ Assets/LevelIntroduction/Level7.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Level7 : LevelSetup {
+	
+	public List<Action> Setup()
+	{
+		List<Action> actions = new List<Action>();
+
+		
+		return actions;
+	}
+	
+	
+}
