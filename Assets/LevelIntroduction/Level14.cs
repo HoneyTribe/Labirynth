729e8012ff2f71c4e547e4064d294b589e25f7b7
HoneyTribe <shaz@honeytribestudios.com>
2014-06-19 13:25:36 +0100

    levels

---
+++ Assets/LevelIntroduction/Level14.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Level14 : LevelSetup {
+	
+	public List<Action> Setup()
+	{
+		List<Action> actions = new List<Action>();
+
+		actions.Add(new MoveCameraAction("JumpContainer(Clone)"));
+		actions.Add(new TextAction(4, "To pick up or drop the anti-grav box tap your bumper while standing on it."));
+		actions.Add(new MoveCameraAction("Lighthouse"));
+		actions.Add(new TextAction(4, "When in the light machine: Aim at the anti-grav box and tap bumper while your friend is standing on it."));
+		actions.Add(new MoveCameraAction("Player1"));
+		actions.Add(new TextAction(4, "You can also force monsters to jump! Use power reserves wisely."));
+
+
+		return actions;
+	}
+	
+	
+}
