dfdfd99db022cc8b93a90a3aef4db897e4ff60ab
endrju3 <endrju3@o2.pl>
2014-11-16 14:43:35 +0000

    Added tutorial arrows

---
+++ Assets/VisibilityController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisibilityController : MonoBehaviour {
+
+	public int level;
+
+	void Start () 
+	{
+		gameObject.SetActive(false);
+		if (LevelFinishedController.instance.getLevel() == level - 1)
+		{
+			gameObject.SetActive(true);
+		}
+	}
+}
