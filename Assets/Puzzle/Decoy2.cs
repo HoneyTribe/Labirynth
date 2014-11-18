cefb657524b05e5cf0375e1262a26a2266f1b99f
endrju3 <endrju3@o2.pl>
2014-11-18 22:30:17 +0000

    Decoy2 reviewed

--- Assets/Puzzle/Decoy2.cs
+++ Assets/Puzzle/Decoy2.cs
@@ -9,27 +9,23 @@ public class Decoy2 : PuzzleTemplate
 		sizeX = 5;
 		sizeZ = 5;
 		
-		extX = 1;
-		extZ = 1;
-		internalSize = sizeZ - 2; 
+		extX = 2;
+		extZ = 4;
+		internalSize = 1; 
 		
-		templateSizeX = 10;
-		templateSizeZ = 10;
-		template = new int[10,10]
-		{
-			{0,2,5,2,2,2,5,2,0,2},
-			{2,0,0,0,0,0,0,0,2,2},
-			{5,0,5,2,4,2,5,0,5,2},
-			{2,0,0,0,0,0,0,0,2,2},
-			{0,2,5,2,6,2,5,2,2,2},
-			{2,0,0,0,6,0,0,0,2,2},
-			{5,0,5,2,2,2,5,0,5,2},
-			{2,0,0,0,0,0,0,0,2,2},
-			{2,2,5,2,2,2,5,2,2,0},
-			{2,2,2,2,2,2,2,2,2,2},
-		};
+		templateSizeX = 9;
+		templateSizeZ = 9;
+		template = new int[9, 9] {  {0,2,5,2,2,2,5,2,0},
+									{2,0,0,0,0,0,0,0,2},
+									{5,0,5,2,4,2,5,0,5},
+									{2,0,0,0,0,0,0,0,2},
+									{2,2,2,2,6,2,2,2,2},
+									{2,0,0,0,0,0,0,0,2},
+									{5,0,5,2,6,2,5,0,5},
+			                        {2,0,0,0,0,0,0,0,2},
+			                        {2,2,5,2,2,2,5,2,2}};
 		
-		extededTemplate = new int[sizeZ * 2, sizeX * 2]; 
+		extededTemplate = new int[sizeZ * 2 -1 , sizeX * 2 -1]; 
 		init ();
 	}
 }
