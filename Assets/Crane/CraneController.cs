452acbd39f1d5a6650fac20dcdb3c8d6eb17ae5c
HoneyTribe <shaz@honeytribestudios.com>
2015-01-11 01:58:52 +0000

    floor text lev 6

--- Assets/Crane/CraneController.cs
+++ Assets/Crane/CraneController.cs
@@ -17,6 +17,9 @@ public class CraneController : MonoBehaviour {
 
 	private bool retracting;
 	private bool entered;
+	public bool publicEnterCrane;
+
+	private FloorInstructions floorInstructions;
 
 	void Start()
 	{
@@ -28,6 +31,7 @@ public class CraneController : MonoBehaviour {
 		grabberController = grabber.GetComponent<CraneGrabberController> ();
 		craneLight = GameObject.Find ("CraneLight");
 		instance = this;
+		floorInstructions = GameObject.Find ("TextInstructionsFloor").GetComponent<FloorInstructions> ();
 	}
 
 	public bool isEntered()
@@ -113,14 +117,18 @@ public class CraneController : MonoBehaviour {
 	public void TurnOn ()
 	{
 		entered = true;
+		publicEnterCrane = true;
 		craneLight.SendMessage ("TurnOn");
+		floorInstructions.ChangeInstructions();
 		retracting = false;
 	}
 	
 	public void TurnOff ()
 	{
 		entered = false;
+		publicEnterCrane = false;
 		craneLight.SendMessage ("TurnOff");
+		floorInstructions.ChangeInstructions();
 		retracting = true;
 	}
 
--- Assets/InstructionsText/FloorInstructions.cs
+++ Assets/InstructionsText/FloorInstructions.cs
@@ -6,10 +6,12 @@ public class FloorInstructions : MonoBehaviour
 	private ScoreController scoreController;
 	private TopLightController topLightController;
 	private LevelFinishedController levelFinishedController;
+	private CraneController craneController;
 	private GameObject arrowCentre;
+	private GameObject arrowRight;
 	public int deadPlayersInstructions;
 	public int decoyInMaze;
-	// Receives variables from DeviceController.cs and ScoreController.cs
+	// Receives variables from DeviceController.cs, ScoreController.cs, CraneController.cs
 	
 	//public Texture instructionsFloor_02;
 	
@@ -17,12 +19,13 @@ public class FloorInstructions : MonoBehaviour
 	{
 		scoreController = GameObject.Find ("GameController").GetComponent<ScoreController> ();
 		topLightController = GameObject.Find ("TopLight").GetComponent<TopLightController> ();
+		craneController = GameObject.Find ("Neck").GetComponent<CraneController> ();
 		levelFinishedController = GameObject.Find ("LevelController").GetComponent<LevelFinishedController> ();
 		arrowCentre = GameObject.Find ("ArrowCentre");
-		//decoyInMaze = 0;
+		arrowRight = GameObject.Find ("ArrowRight");
 	}
 
-	//Called from ScoreController.cs and TopLightController.cs and DeviceController.cs
+	//Called from ScoreController.cs, TopLightController.cs, DeviceController.cs, CraneController.cs
 	public void ChangeInstructions ()
 	{	
 		if(levelFinishedController.publicLevel == 0)
@@ -33,12 +36,11 @@ public class FloorInstructions : MonoBehaviour
 
 				if (scoreController.publicScore > 0)
 				{
-					//renderer.material.mainTexture = instructionsFloor_02;
-					GetComponentInChildren<TextMesh>().text = "Hold action-1 to exit the machine.";
+					GetComponentInChildren<TextMesh>().text = "Hold action-1 to exit the machine";
 				}
 				else
 				{
-					GetComponentInChildren<TextMesh>().text = "Hold action-1 to exit the machine.";
+					GetComponentInChildren<TextMesh>().text = "Hold action-1 to exit the machine";
 				}
 			}
 
@@ -61,7 +63,7 @@ public class FloorInstructions : MonoBehaviour
 			{
 				if(decoyInMaze ==0)
 				{
-					GetComponentInChildren<TextMesh>().text = "Maze-runners: Tap action-1 to move the Decoy.";
+					GetComponentInChildren<TextMesh>().text = "Maze-runners: Tap action-1 to move the Decoy";
 				}
 				else if (decoyInMaze ==1 && topLightController.enterLight == true)
 				{
@@ -74,11 +76,23 @@ public class FloorInstructions : MonoBehaviour
 			}
 			else //someone dead
 			{
-				GetComponentInChildren<TextMesh>().text = "Revive your fallen friends by touching them.";
+				GetComponentInChildren<TextMesh>().text = "Revive your fallen friends by touching them";
 			}
 		}
+		else if(levelFinishedController.publicLevel == 5)
+		{
+			if(craneController.publicEnterCrane == true)
+			{
+				GetComponentInChildren<TextMesh>().text = "Extend with up and down. Tap action-1 to pick up/drop things.";
+				arrowRight.transform.position = new Vector3(4, -0.5f, -13);
+			}
+			else
+			{
+				GetComponentInChildren<TextMesh>().text = "Walk into the Grabber         entrance";
+				arrowRight.transform.position = new Vector3(4, 0.5f, -13);
+			}
+		}	
 
-	
 	}
 
 	// Called in Introduction Controller.cs in public void StopIntroduction(bool stopping) at approx line 132
@@ -89,9 +103,15 @@ public class FloorInstructions : MonoBehaviour
 			GetComponentInChildren<TextMesh>().text = "Walk into          the light";
 			arrowCentre.transform.position = new Vector3(0, 0.5f, -13);
 			}
-
+		else if (levelFinishedController.publicLevel == 5)
+		{
+			//GetComponentInChildren<TextMesh>().alignment = TextAlignment.Left;
+			GetComponentInChildren<TextMesh>().text = "Walk into the Grabber         entrance";
+			arrowRight.transform.position = new Vector3(4, 0.5f, -13);
+		}
 	}
 
 
 
 }
+//renderer.material.mainTexture = instructionsFloor_02;
