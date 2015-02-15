5d7136dc8210c0ebf0d8c125a94401bc677da152
HoneyTribe <shaz@honeytribestudios.com>
2015-02-15 15:36:16 +0000

    playerSelectBack

---
+++ Assets/LevelIntroduction/Level18.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Level18 : LevelSetup {
+	
+	public List<Action> Setup()
+	{
+		List<Action> actions = new List<Action>();
+		
+		actions.Add (new ChangeCamAction());
+		actions.Add(new MoveCameraAction("Drone"));
+		actions.Add(new TextAction(4, "The Teleport Drone is broken... and the Decoy too. But..."));
+		actions.Add(new MoveCameraAction("Drone"));
+		actions.Add(new TextAction(4, "We installed a new abilty! Drop stun bombs by tapping action-2."));
+		
+		return actions;
+	}
+}
