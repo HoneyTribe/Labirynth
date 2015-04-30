f7f8fd6e8d591589bdd57061088f55dc60c396ca
HoneyTribe <shaz@honeytribestudios.com>
2015-04-30 23:57:21 +0100

    level 3 adjust

---
+++ Assets/LevelIntroduction/Level9.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Level9 : LevelSetup {
+	
+	public List<Action> Setup()
+	{
+		List<Action> actions = new List<Action>();
+
+		actions.Add(new ImageAction("tut_06"));
+		actions.Add(new MoveCameraAction("Lighthouse"));
+		actions.Add(new TextAction(4, "Upgrade! You have a Grabber!"));
+		actions.Add(new MoveCameraAction("Crane"));
+		actions.Add(new TextAction(4, "Now you can pick up and drop things."));
+		actions.Add(new MoveCameraAction("Crane"));
+		actions.Add(new TextAction(4, "Picking up costs power. Don't drain the power too soon!"));
+
+		return actions;
+	}
+	
+	
+}
