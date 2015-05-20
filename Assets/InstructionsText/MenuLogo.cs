c5f1faebac735806c6af90e7447b5cf4c58afa2f
HoneyTribe <shaz@honeytribestudios.com>
2015-05-20 16:12:50 +0100

    menu logo dynamic position

---
+++ Assets/InstructionsText/MenuLogo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuLogo : MonoBehaviour {
+	
+	void Start ()
+	{
+		if(LevelFinishedController.instance.getMaxLevel()>0)
+		{
+			this.transform.position = new Vector3(-20f, 0.02f, 1f);
+		}
+		else
+		{
+			this.transform.position = new Vector3(2f, 0.02f, 1f);
+		}
+	}
+
+}
