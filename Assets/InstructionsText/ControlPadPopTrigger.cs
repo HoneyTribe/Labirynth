48279d6e80f5c0b8646278579245f08d8343fcd6
HoneyTribe <shaz@honeytribestudios.com>
2015-05-27 00:34:26 +0100

    Decoy alert for level 4

---
+++ Assets/InstructionsText/ControlPadPopTrigger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ControlPadPopTrigger : MonoBehaviour 
+{
+	
+	private static int openHash = Animator.StringToHash ("Open");
+	private static int closeHash = Animator.StringToHash ("Close");
+	private GameObject controlPadPop;
+	private Animator anim;
+	
+	void Start()
+	{
+		//controlPadPop = GameObject.Find ("ControlPadPop");
+		anim = GameObject.Find ("ControlPadPop").GetComponent<Animator> ();
+	}
+	
+	public void OnTriggerEnter(Collider currentCollider)
+	{
+		if ((currentCollider.tag  == "Player"))
+		{
+			anim.SetTrigger(openHash);
+		}
+	}
+	public void OnTriggerExit(Collider currentCollider)
+	{
+		if ((currentCollider.tag  == "Player"))
+		{
+			anim.SetTrigger(closeHash);
+		}
+	}
+}
