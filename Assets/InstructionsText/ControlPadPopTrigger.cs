9da5ceac1b3c19d371e14a66b563cc01b0ec81b2
HoneyTribe <shaz@honeytribestudios.com>
2015-05-27 11:43:25 +0100

    Adjust decoy prompt on lev 4

--- Assets/InstructionsText/ControlPadPopTrigger.cs
+++ Assets/InstructionsText/ControlPadPopTrigger.cs
@@ -7,13 +7,16 @@ public class ControlPadPopTrigger : MonoBehaviour
 	
 	private static int openHash = Animator.StringToHash ("Open");
 	private static int closeHash = Animator.StringToHash ("Close");
-	private GameObject controlPadPop;
+	private static int fadeInHash = Animator.StringToHash ("FadeIn");
+	private static int fadeOutHash = Animator.StringToHash ("FadeOut");
 	private Animator anim;
+	private Animator anim2;
 	
 	void Start()
 	{
 		//controlPadPop = GameObject.Find ("ControlPadPop");
 		anim = GameObject.Find ("ControlPadPop").GetComponent<Animator> ();
+		anim2 = GameObject.Find ("ControlPadPopTrigger").GetComponent<Animator> ();
 	}
 	
 	public void OnTriggerEnter(Collider currentCollider)
@@ -21,6 +24,7 @@ public class ControlPadPopTrigger : MonoBehaviour
 		if ((currentCollider.tag  == "Player"))
 		{
 			anim.SetTrigger(openHash);
+			anim2.SetTrigger(fadeOutHash);
 		}
 	}
 	public void OnTriggerExit(Collider currentCollider)
@@ -28,6 +32,7 @@ public class ControlPadPopTrigger : MonoBehaviour
 		if ((currentCollider.tag  == "Player"))
 		{
 			anim.SetTrigger(closeHash);
+			anim2.SetTrigger(fadeInHash);
 		}
 	}
 }
