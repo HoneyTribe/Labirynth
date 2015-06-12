400f34fee999d357d0901e46b90c3a84e723440e
HoneyTribe <shaz@honeytribestudios.com>
2015-06-12 02:52:50 +0100

    Trail Renderer added to all monster prefabs

--- Assets/Effects/DecoyTrail.cs
+++ Assets/Effects/DecoyTrail.cs
@@ -4,61 +4,74 @@ using System.Collections.Generic;
 
 public class DecoyTrail : MonoBehaviour 
 {
-	
+
 	//public static DistractLine instance;
 	
 	private GameObject device;
 	private ParticleSystem effect;
-	private float speed = 20.0f;
+	public float speed = 25.0f;
 	private bool resetTime = false;
 	private float startTime;
-	private int closeDistance = 1;
+	public float closeDistance = 0.1f;
+	public float mummyOffset = 3.0f;
+	public float decoyOffset = 0.0f;
+	public float constant = 2.0f;
+	public float trailDuration = 2.0f;
+	public float endDuration = 0.2f;
+	Transform myTransform;
+	private TrailRenderer trail;
+	//Transform parentTransform;
+	//Transform deviceTransform;
+	Vector3 myTempPos;
 	
 	void Start()
 	{
 		//instance = this;
 		device = GameObject.Find ("Device");
+		myTransform = this.transform;
+		trail = GetComponent<TrailRenderer>();
 		//effect = transform.particleSystem;
 	}
-	
+
 	void Update()
 	{
 		if(transform.parent.GetComponent<AbstractMonsterController>().getTimeLeft() > 0 )
 		{
-			transform.position = transform.parent.position;
-			//effect.enableEmission = true;
 
+			//trail.enabled = true;
+			trail.time = trailDuration;
 			if(resetTime == false)
 			{
 				resetTimer();
 			}
 
-			if(Vector3.Distance(transform.position, device.transform.position) > closeDistance)
+			if(Vector3.Distance(myTransform.position, device.transform.position) > closeDistance)
 			{
 				float dist = Vector3.Distance(device.transform.position, transform.parent.position);
 				float distCovered = (Time.time - startTime) * speed;
 				float fracJourney = distCovered / dist;
 
-				transform.position = Vector3.Lerp(transform.parent.position, device.transform.position, fracJourney);
+				//this works// transform.position = Vector3.Lerp(transform.parent.position, device.transform.position, fracJourney) ;
+
+				myTempPos = myTransform.position;
+				myTempPos.x = Mathf.Lerp(transform.parent.position.x, device.transform.position.x, fracJourney) ;
+				myTempPos.y = Mathf.Lerp(transform.parent.position.y + mummyOffset, device.transform.position.y + decoyOffset, fracJourney) ;
+				//myTempPos.y = (Mathf.Sin(constant * (Time.time - startTime) * Mathf.PI)+ 1) / 2 + mummyOffset;
+				myTempPos.z = Mathf.Lerp(transform.parent.position.z, device.transform.position.z, fracJourney) ;
+				myTransform.position = myTempPos;
 			}
-			if(Vector3.Distance(transform.position, device.transform.position) <= closeDistance)
+			if(Vector3.Distance(myTransform.position, device.transform.position) <= closeDistance)
 			{
-				transform.position = transform.parent.position;
-				//transform.position = new Vector3(0,0,0);
+				myTransform.position = transform.parent.position;
 				resetTime = false;
 			}
-			
-			//float dist = Vector3.Distance(device.transform.position, transform.parent.position);
-			//Vector3 pos = transform.parent.position + (device.transform.position - transform.parent.position) / 2;
-			
-			//transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, dist * 0.5f);
-			//transform.position = new Vector3(pos.x, pos.y, pos.z);
-			//transform.LookAt(transform.parent);
+
 		}
 		else
 		{
-			//transform.position = new Vector3(0, -10, 0);
-			//effect.enableEmission = false;
+			myTransform.position = transform.parent.position;
+			//trail.enabled = false;
+			trail.time = endDuration;
 		}
 		
 	}
@@ -68,4 +81,5 @@ public class DecoyTrail : MonoBehaviour
 		startTime = Time.time;
 		resetTime = true;
 	}
+
 }
