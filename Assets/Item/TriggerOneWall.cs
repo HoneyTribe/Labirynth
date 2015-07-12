6709056396cd702f49329549d63c847fe5471f93
endrju3 <endrju3@o2.pl>
2015-07-12 13:03:37 +0100

    Added crap code??

---
+++ Assets/Item/TriggerOneWall.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerOneWall : MonoBehaviour
+{
+
+	public void OnTriggerEnter(Collider currentCollider)
+	{
+		if ((currentCollider.tag == "Monster") || (currentCollider.tag  == "Player"))
+		{
+			AudioController.instance.Play("021_BlockMovesB");
+			gameObject.transform.Translate (0, -1.1f, 0);
+		}
+	}
+}
